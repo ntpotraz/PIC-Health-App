703ef8fcc62443491f42e158aa492f37d12f7564
ntpotraz <60411602+ntpotraz@users.noreply.github.com>
2024-10-22 11:33:30 -0700

    Changed Homepage back to say Homepage

--- components/HomeScreen.jsx
+++ components/HomeScreen.jsx
@@ -4,7 +4,7 @@ import { View, Text, StyleSheet } from 'react-native';
 export default function HomeScreen() {
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>Hello World</Text>
+      <Text style={styles.text}>Homepage</Text>
     </View>
   );
 }
