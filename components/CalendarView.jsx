ee709048fc4e9a1d9f546213b5a787afa9a6370c
Salter11 <salte010@csusm.edu>
2024-11-06 14:52:45 -0800

    color coded events

--- components/CalendarView.jsx
+++ components/CalendarView.jsx
@@ -1,12 +1,39 @@
-import { View, Text, TouchableOpacity, StyleSheet } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { View, Text, StyleSheet } from 'react-native';
 import { Calendar } from 'react-native-calendars';
 
 const CalendarView = ({ onEventPress, events, selectedCalendars }) => {
+  const [markedDates, setMarkedDates] = useState({});
 
-  const markedDates = Object.keys(events).reduce((acc, date) => {
-    acc[date] = { marked: true };
-    return acc;
-  }, {});
+  useEffect(() => {
+    const formatEvents = (events) => {
+      const calendarColors = {
+        'pichealthtest@gmail.com': 'red',
+        'f98eb9b3491ce0f74ae3d3dca31849eedcd596b5f7a7cb5a8604f05932d11128@group.calendar.google.com': 'blue',
+      };
+
+      const formattedEvents = Object.keys(events).reduce((acc, date) => {
+        const eventList = events[date];
+        const dots = eventList.map(event => {
+          const calendarId = event.organizer.email;
+          const color = calendarColors[calendarId] || 'gray';
+          return { color };
+        });
+
+        acc[date] = { dots };
+        return acc;
+      }, {});
+
+      return formattedEvents;
+    };
+
+    if (selectedCalendars.length > 0) {
+      const formattedEvents = formatEvents(events);
+      setMarkedDates(formattedEvents);
+    } else {
+      setMarkedDates({});
+    }
+  }, [events, selectedCalendars]);
 
   return (
     <View style={styles.container}>
@@ -16,26 +43,13 @@ const CalendarView = ({ onEventPress, events, selectedCalendars }) => {
         ) : (
           <Calendar
             markedDates={markedDates}
+            markingType={'multi-dot'}
             onDayPress={(day) => {
               const selectedDateEvents = events[day.dateString] || [];
               selectedDateEvents.forEach(event => onEventPress(event));
             }}
           />
         )}
-      </View>
-      
-      {/* Button Section */}
-      <View style={styles.middleBtns}>
-        <TouchableOpacity>
-          <View style={styles.wellnessSOS}>
-            <Text style={{ fontSize: 20, fontWeight: "bold" }}>Wellness Check</Text>
-          </View>
-        </TouchableOpacity>
-        <TouchableOpacity>
-          <View style={styles.wellnessSOS}>
-            <Text style={{ fontSize: 20, fontWeight: "bold" }}>SOS</Text>
-          </View>
-        </TouchableOpacity>
       </View>
     </View>
   );
@@ -49,31 +63,6 @@ const styles = StyleSheet.create({
     justifyContent: "space-between",
   },
   calendarContainer: {
-  },
-  eventItem: {
-    padding: 10,
-  },
-  middleBtns: {
-    flexDirection: "row",
-    justifyContent: "space-around",
-    marginTop: 20, // Add space between calendar and buttons
-  },
-  wellnessSOS: {
-    width: 200,
-    height: 50,
-    justifyContent: "center",
-    alignItems: "center",
-    borderRadius: 10,
-    backgroundColor: "rgba(255, 255, 255, 0.6)",
-    marginBottom: 20,
-  },
-  dropdown: {
-    backgroundColor: '#fff', // White background for the dropdown
-  },
-  dropdownBox: {
-    backgroundColor: '#fff', // White background for the box
-    borderColor: '#fff', // Optional: Add border color
-    borderRadius:0
   },
   noEventsText: {
     fontSize: 16,
