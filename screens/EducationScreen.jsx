63eacdb34a1255072baba8cde813eab752559c05
Michael <mwduggan11@gmail.com>
2024-11-14 14:39:11 -0800

    Changed Online Courses into CSUSM APIDA along with a description

--- screens/EducationScreen.jsx
+++ screens/EducationScreen.jsx
@@ -29,12 +29,11 @@ export default function EducationScreen() {
             </View>
 
             <View style={textBox.container}>
-              <Text style={textBox.title}>Online Courses</Text>
+              <Text style={textBox.title}>CSUSM APIDA</Text>
               <Text style={textBox.text}>
-                Lorem Ipsum is simply dummy text of the printing and typesetting industry.
-                Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
-                when an unknown printer took a galley of type and scrambled it to make a type
-                specimen book.
+              Success initiative for Asian, Pacific Islander, and 
+              Desi American students in California State University, 
+              San Marcos
               </Text>
               <TouchableOpacity onPress={() => Linking.openURL('https://google.com')}>
                 <Text style={textBox.link}>
