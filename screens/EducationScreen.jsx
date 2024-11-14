6347269066438d331da5157db729396d77e01e29
Michael <mwduggan11@gmail.com>
2024-11-14 14:45:56 -0800

    Changed Workshops into "How to apply to college?""

--- screens/EducationScreen.jsx
+++ screens/EducationScreen.jsx
@@ -43,7 +43,7 @@ export default function EducationScreen() {
             </View>
 
             <View style={textBox.container}>
-              <Text style={textBox.title}>Workshops</Text>
+              <Text style={textBox.title}>How to apply to college?</Text>
               <Text style={textBox.text}>
                 Lorem Ipsum is simply dummy text of the printing and typesetting industry.
                 Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
