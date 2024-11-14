0cb1bdaebf96dd93b72339c023646ad27a2aff77
Michael <mwduggan11@gmail.com>
2024-11-14 15:16:15 -0800

    Fixed minor syntax error to the About Us page

--- screens/AboutUs.jsx
+++ screens/AboutUs.jsx
@@ -42,7 +42,6 @@ export default function HomeScreen() {
                   to promoting wellness and resilience by addressing the unique health needs
                   and disparities our Pasifika communities endure and navigate every day.
                 </Text>
-              </View>
               <Text style={textBox.text}>
                   
                 </Text>
