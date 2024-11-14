8c4ea08e66aa6a63302cb6c507b3e75690aa4cba
Michael <mwduggan11@gmail.com>
2024-11-14 14:29:12 -0800

    Added a text container with the title Mindullness and dummy text

--- screens/HealthScreen.jsx
+++ screens/HealthScreen.jsx
@@ -71,6 +71,21 @@ export default function HealthScreen() {
             </TouchableOpacity>
           </View>
 
+          <View style={textBox.container}>
+            <Text style={textBox.title}>Mindfullness Resources</Text>
+            <Text style={textBox.text}>
+            Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
+            Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
+            when an unknown printer took a galley of type and scrambled it to make a type 
+            specimen book.
+            </Text>
+            <TouchableOpacity onPress={() => Linking.openURL('https://google.com')}>
+              <Text style={textBox.link}>
+                Hyperlink
+              </Text>
+            </TouchableOpacity>
+          </View>
+
         </View>
       </ScrollView>
       </ImageBackground>
