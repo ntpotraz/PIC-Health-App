4e45e5696b121d864c1a963a301b283adce859ea
Michael <mwduggan11@gmail.com>
2024-11-14 14:20:22 -0800

    Vista Community Clinic and its link and description

--- screens/HealthScreen.jsx
+++ screens/HealthScreen.jsx
@@ -30,16 +30,13 @@ export default function HealthScreen() {
           </View>
 
           <View style={textBox.container}>
-            <Text style={textBox.title}> Community Health Clinics</Text>
+            <Text style={textBox.title}>Vista Community Clinic</Text>
             <Text style={textBox.text}>
-            Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
-            Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
-            when an unknown printer took a galley of type and scrambled it to make a type 
-            specimen book.
+             Health care provider in Southern California in general
             </Text>
-            <TouchableOpacity onPress={() => Linking.openURL('https://google.com')}>
+            <TouchableOpacity onPress={() => Linking.openURL('https://www.vistacommunityclinic.org/about/')}>
               <Text style={textBox.link}>
-                Hyperlink
+               https://www.vistacommunityclinic.org/about/
               </Text>
             </TouchableOpacity>
           </View>
