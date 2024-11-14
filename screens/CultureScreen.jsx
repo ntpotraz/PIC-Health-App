a5c33d8c0a246482b43c6026f514816894d6b018
Michael <mwduggan11@gmail.com>
2024-11-14 15:01:55 -0800

    Changed the title of Movement Classes

--- screens/CultureScreen.jsx
+++ screens/CultureScreen.jsx
@@ -44,7 +44,7 @@ export default function CultureScreen() {
             </View>
 
             <View style={textBox.container}>
-              <Text style={textBox.title}>Movement Classes</Text>
+              <Text style={textBox.title}>Movement Class</Text>
               <Text style={textBox.text}>
                 Lorem Ipsum is simply dummy text of the printing and typesetting industry.
                 Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
