47028b5ef4570c55bef407c710d072d756a056e3
Michael <mwduggan11@gmail.com>
2024-11-29 13:43:28 -0800

    Fixed the links to be viewed on the app instead being directed to a browser

--- screens/HealthScreen.jsx
+++ screens/HealthScreen.jsx
@@ -1,10 +1,29 @@
-import React from 'react';
-import { ImageBackground, Text, View, StyleSheet, ScrollView, Linking, TouchableOpacity } from 'react-native';
+import React, {useState} from 'react';
+import { ImageBackground, Text, View, StyleSheet, ScrollView, Linking, TouchableOpacity, Platform } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import Header from '../components/Header';
 import Disclaimer from '../components/Disclaimer';
+import WebViewModal from '../components/WebViewModal';
 
 const HealthScreen = () => {
+  const [modalConfig, setModalConfig] = useState({isVisible: false, url: ''});
+
+  const callWebView = (url) => {
+    Platform.OS === 'web' ?
+      Linking.openURL(url) :
+      setModalConfig({
+        isVisible: true,
+        url: url
+      });
+  };
+
+  const closeModal = () => {
+    setModalConfig(prev => ({
+      ...prev,
+      isVisible: false
+    }));
+  }
+
   return (
     <SafeAreaView edges={['top']} style={styles.container}>
       <ImageBackground
@@ -21,7 +40,7 @@ const HealthScreen = () => {
             <View style={textBox.container}>
               <Text style={textBox.title}>Diabetes Screening</Text>
               <Text style={textBox.text}>This test is used to identify individuals who may have diabetes or are at risk of developing the condition, often before they show any noticeable symptoms.</Text>
-              <TouchableOpacity onPress={() => Linking.openURL('https://www.cdc.gov/prediabetes/risktest/index.html')}>
+              <TouchableOpacity onPress={() => callWebView('https://www.cdc.gov/prediabetes/risktest/index.html')}>
                 <Text style={textBox.link}>Risk Test</Text>
               </TouchableOpacity>
             </View>
@@ -29,10 +48,10 @@ const HealthScreen = () => {
             <View style={textBox.container}>
               <Text style={textBox.title}>Mental Wellness Resources</Text>
               <Text style={textBox.text}>Mental health resources are designed to support individuals in managing their emotional, psychological, and social well-being. Their purpose is to provide tools, guidance, and services to help people understand, cope with, and address mental challenges.</Text>
-              <TouchableOpacity onPress={() => Linking.openURL('https://screening.mhanational.org/mental-health-101/')}>
+              <TouchableOpacity onPress={() => callWebView('https://screening.mhanational.org/mental-health-101/')}>
                 <Text style={textBox.link}>Mental Health 101</Text>
               </TouchableOpacity>
-              <TouchableOpacity onPress={() => Linking.openURL('https://screening.mhanational.org/screening-tools/depression/?ref')}>
+              <TouchableOpacity onPress={() => callWebView('https://screening.mhanational.org/screening-tools/depression/?ref')}>
                 <Text style={textBox.link}>Depression Screening</Text>
               </TouchableOpacity>
             </View>
@@ -49,7 +68,7 @@ const HealthScreen = () => {
                 to everyone in the communities they serve, regardless of income
                 or insurance status.
               </Text>
-              <TouchableOpacity onPress={() => Linking.openURL('https://truecare.org/')}>
+              <TouchableOpacity onPress={() => callWebView('https://truecare.org/')}>
                 <Text style={textBox.link}>Homepage</Text>
               </TouchableOpacity>
             </View>
@@ -60,7 +79,7 @@ const HealthScreen = () => {
                 Valuable Connected Care: Meeting the health and wellness needs
                 of our community. A community where every person chooses health.
               </Text>
-              <TouchableOpacity onPress={() => Linking.openURL('https://www.vistacommunityclinic.org/')}>
+              <TouchableOpacity onPress={() => callWebView('https://www.vistacommunityclinic.org/')}>
                 <Text style={textBox.link}>Homepage</Text>
               </TouchableOpacity>
             </View>
@@ -69,7 +88,7 @@ const HealthScreen = () => {
             <View style={textBox.container}>
               <Text style={textBox.title}>Mindfulness Resources</Text>
               <Text style={textBox.text}>Mindfulness resources are designed to help individuals cultivate awareness, presence, and acceptance of the present moment. These resources aim to promote mental and emotional well-being by teaching practices that encourage mindfulness—essentially the practice of paying attention in a particular way: on purpose, in the present moment, and without judgment.</Text>
-              <TouchableOpacity onPress={() => Linking.openURL('https://www.csusm.edu/mindfulcsusm/resources/index.html')}>
+              <TouchableOpacity onPress={() => callWebView('https://www.csusm.edu/mindfulcsusm/resources/index.html')}>
                 <Text style={textBox.link}>Mindful CSUSM</Text>
               </TouchableOpacity>
             </View>
@@ -77,7 +96,7 @@ const HealthScreen = () => {
             <View style={textBox.container}>
               <Text style={textBox.title}>Movement & Nutrition</Text>
               <Text style={textBox.text}>Movement and nutrition resources empower individuals to take charge of their health by providing the tools, knowledge, and guidance they need to make informed decisions about physical activity and dietary choices. The combined effect of regular movement and proper nutrition supports overall well-being, reduces disease risk, enhances mental health, improves energy, and promotes longevity.</Text>
-              <TouchableOpacity onPress={() => Linking.openURL('https://www.cdc.gov/cdi/indicator-definitions/npao.html')}>
+              <TouchableOpacity onPress={() => callWebView('https://www.cdc.gov/cdi/indicator-definitions/npao.html')}>
                 <Text style={textBox.link}>Chronic Disease Indicators</Text>
               </TouchableOpacity>
             </View>
@@ -88,13 +107,14 @@ const HealthScreen = () => {
                 Screening for cancer allows you to improve prognosis,
                 reduce mortality, identify early detection, and give you peace of mind.
               </Text>
-              <TouchableOpacity onPress={() => Linking.openURL('https://www.cancer.org/cancer/screening/american-cancer-society-guidelines-for-the-early-detection-of-cancer.html')}>
+              <TouchableOpacity onPress={() => callWebView('https://www.cancer.org/cancer/screening/american-cancer-society-guidelines-for-the-early-detection-of-cancer.html')}>
                 <Text style={textBox.link}>Cancer Screening</Text>
               </TouchableOpacity>
             </View>
           </View>
         </ScrollView>
       </ImageBackground>
+      <WebViewModal url={modalConfig.url} isVisible={modalConfig.isVisible} onClose={closeModal} />
     </SafeAreaView>
   );
 }
